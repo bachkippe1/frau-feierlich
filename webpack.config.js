3d65740881de833aaeaecbf67fd14c6140e1e5ef
Valentin Rittmann <valentinrittmann@me.com>
2018-09-30 21:14:29 +0200

    setup webpack environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,86 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-  entry:  {
-      app: './src/index.js'
+  entry: {
+   'main' : [
+    './src/js/app.js',
+    './src/css/main.scss',
+   ]   
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    // publicPath: '/dist'
   },
   watch: true,
+  module: {
+      rules: [  
+          {
+              test: /\.js$/,
+              use: [
+                  {
+                      loader: 'babel-loader',
+                      options: {
+                          presets: ['@babel/preset-env']
+                      }
+                  }
+              ]
+          },     
+          {
+              test: /\.scss$/,
+              use: ExtractTextPlugin.extract({
+                  use: [
+                      'css-loader',
+                      'sass-loader'
+                  ]
+              })
+              
+          },
+          {
+            test: /\.(png|jpg|gif|svg|eot|ttf)$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'img/',
+                    sourcePath: 'img/'
+                }
+            }]
+          },
+          {
+            test: /\.(woff|woff2)$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'fonts/',
+                    sourcePath: 'fonts/'
+                }
+            }]
+          },
+          {
+              test: /\.html$/,
+              use: ['html-loader']
+          }
+      ]
+  },  
   plugins: [
     new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-        hash: true,
-        title: 'Production',
-        myPageHeader: 'Hello Frau Feierlich',
-        template: './src/index.html',
-        filename: 'index.html'
-    }),
     new BrowserSyncPlugin({
         host: 'localhost',
         port: 3000,
         server: { baseDir: ['dist'] }
+    }),
+    new ExtractTextPlugin({
+        filename: 'style.css'
+    }),
+    new HtmlWebpackPlugin({
+        template: 'src/index.html'
     })
   ],
-  module: {
-      rules: [
-          {
-              test: /\.css$/,
-              use: [
-                  'style-loader',
-                  'css-loader'
-              ]
-          }
-      ]
-  },
 };
